e49148c870a34aa6ec93fbfd7763a1f3384bc707
FabrizioRodriguez-Tech <alexander.rodriguez@tecdesoftware.edu.mx>
2026-06-30 22:58:38 -0600

    feat: reproductor de audio. -fix: minimización arrastable

--- Gnosis.Business/Models/CatalogoFondos.cs
+++ Gnosis.Business/Models/CatalogoFondos.cs
@@ -6,10 +6,11 @@ namespace Gnosis.Business.Models
         public static readonly List<FondoVideo> Disponibles = new()
         {
             new FondoVideo { Id = "lluvia-tokio",      Nombre = "Lluvia en Tokio",        YoutubeVideoId = "DvfSPb8VWC4", ColorPlaceholder = "#3a4a5a" },
-            new FondoVideo { Id = "fondo-2",            Nombre = "Luxury Miami Apartment",                YoutubeVideoId = "QUqhgZjrrsE", ColorPlaceholder = "#4a5a6a" },
+            new FondoVideo { Id = "Luxury-miami",            Nombre = "Luxury Miami Apartment",                YoutubeVideoId = "QUqhgZjrrsE", ColorPlaceholder = "#4a5a6a" },
             new FondoVideo { Id = "carretera-lluvia",   Nombre = "Carretera con lluvia",   YoutubeVideoId = "azYAiBWRZA0", ColorPlaceholder = "#2a3a4a" },
-            new FondoVideo { Id = "fondo-4",            Nombre = "NewYork Skyline",                YoutubeVideoId = "Qe0aZ26eknU", ColorPlaceholder = "#3a3a5a" },
-            new FondoVideo { Id = "shibuya-noche",      Nombre = "Caminata en Shibuya",    YoutubeVideoId = "2y2Z06hVmWE", ColorPlaceholder = "#2a2a3a" },
+            new FondoVideo { Id = "NewYork-Skyline",            Nombre = "NewYork Skyline",                YoutubeVideoId = "Qe0aZ26eknU", ColorPlaceholder = "#3a3a5a" },
+            new FondoVideo { Id = "Shibuya Noche",      Nombre = "Caminata en Shibuya",    YoutubeVideoId = "2y2Z06hVmWE", ColorPlaceholder = "#2a2a3a" },
+            new FondoVideo { Id = "Osaka-night-drive",      Nombre = "Osaka Night Drive",    YoutubeVideoId = "ovD7QlzCCoU", ColorPlaceholder = "#2a2a3a" }
         };
 
         public static FondoVideo Predeterminado => Disponibles[0];
